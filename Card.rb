a3c7f62fc7c5ce2b4ca0dcdd9e5c04193bf65eb6
Stephen Clark <stephenmclark527@gmail.com>
2019-09-14 16:08:08 -0400

    Initial code for Card class

---
+++ Card.rb
@@ -0,0 +1,27 @@
+class Card
+
+  attr_reader :face_value
+
+  def initialize
+    @face_value = ("A".."Z").to_a.sample  #sets @face_value to random capital letter
+    @face_up = true   #each card starts face up at beginning of game
+  end
+
+  def display_card   #only shows card's face value when @face_up == true
+    if @face_up
+      puts @face_value
+    end
+  end
+
+  def ==(other)  #evaluates card equality based on @face_value
+    self.face_value == other.face_value
+  end
+
+  def hide
+    @face_up = false
+  end
+
+  def reveal
+    @face_up = true
+  end
+end
